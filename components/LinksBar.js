2de29001eade674f10e9b17450d078e5812aab21
JeffreytheCoder <y2397371802@gmail.com>
2022-03-26 18:13:32 -0700

    avatar popover

--- components/LinksBar.js
+++ components/LinksBar.js
@@ -1,5 +1,12 @@
-import React from 'react';
-import { Avatar, IconButton } from '@mui/material';
+import React, { useState, useRef } from 'react';
+import {
+  Avatar,
+  IconButton,
+  Typography,
+  Box,
+  ButtonBase,
+  Button,
+} from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
@@ -8,6 +15,8 @@ import { styled } from '@mui/material/styles';
 import { Badge } from '@mui/material';
 import { Popover } from '@mui/material';
 import Link from 'next/link';
+import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from '@mui/icons-material/Email';
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
   '& .MuiBadge-badge': {
@@ -39,15 +48,79 @@ const StyledBadge = styled(Badge)(({ theme }) => ({
 }));
 
 function LinksBar() {
+  const avatarRef = useRef(null);
+  const [popoverOpen, setPopoverOpen] = useState(false);
+
+  const handlePopoverClose = () => {
+    setPopoverOpen(false);
+  };
+
   return (
     <>
-      <StyledBadge
-        overlap="circular"
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        variant="dot"
+      <Box
+        component={ButtonBase}
+        onClick={() => setPopoverOpen(!popoverOpen)}
+        ref={avatarRef}
       >
-        <Avatar src="/avatar.jpg" sx={{ width: 50, height: 50 }} />
-      </StyledBadge>
+        <StyledBadge
+          overlap="circular"
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          variant="dot"
+        >
+          <Avatar src="/avatar.jpg" sx={{ width: 50, height: 50 }} />
+        </StyledBadge>
+      </Box>
+
+      <Popover
+        open={popoverOpen}
+        onClose={handlePopoverClose}
+        anchorEl={avatarRef.current}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        PaperProps={{
+          sx: {
+            border: 1,
+            borderColor: grey[200],
+            padding: 2,
+            marginLeft: 2,
+            borderRadius: '5%',
+          },
+        }}
+        elevation={0}
+      >
+        <Box flex flexDirection="column">
+          <Typography variant="h6">Jeffrey Yu</Typography>
+          <Box
+            sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
+          >
+            <EmailIcon />
+            <Typography
+              variant="span"
+              sx={{ marginLeft: '8px', marginTop: '5px' }}
+            >
+              jeffreyzepengyu@g.ucla.edu
+            </Typography>
+          </Box>
+          <Box
+            sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
+          >
+            <PhoneIcon />
+            <Typography
+              variant="span"
+              sx={{ marginLeft: '8px', marginTop: '5px' }}
+            >
+              213-468-2703
+            </Typography>
+          </Box>
+          <a
+            href="https://drive.google.com/file/d/1WHlytWajUt7juF6nKY5KhfabCXS_Fr1z/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button>See my resume</Button>
+          </a>
+        </Box>
+      </Popover>
+
       <a
         href="https://www.linkedin.com/in/jeffrey-zepeng-yu/"
         target="_blank"
